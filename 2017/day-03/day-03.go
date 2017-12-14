d993b33ce806e64aaba37401e0a0c6ecfb96fb27
Jonathan Duncan <jonathan@nacnud.com>
2017-12-13 23:18:23 -0700

    day 03 part 2

--- 2017/day-03/day-03.go
+++ 2017/day-03/day-03.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
+
+type Coordinate struct {
+	x     int
+	y     int
+	value int
+}
 
 type Day03 struct {
 	direction                  string
@@ -12,6 +17,7 @@ type Day03 struct {
 	lastNumberOfSteps          int
 	maxStepsInThisDirection    int
 	stepsInTheCurrentDirection int
+	memoryMap                  []Coordinate
 }
 
 func NewDay03() *Day03 {
@@ -22,12 +28,16 @@ func NewDay03() *Day03 {
 		lastNumberOfSteps:          0,
 		maxStepsInThisDirection:    1,
 		stepsInTheCurrentDirection: 0,
+		memoryMap:                  []Coordinate{},
 	}
 }
 
 func main() {
 	day := NewDay03()
-	for gridSquare := 1; gridSquare < 289326; gridSquare++ {
+	for gridSquare := 1; gridSquare <= 60; gridSquare++ {
+		square := Coordinate{x: day.coordinateX, y: day.coordinateY}
+		square.value = day.checkNeighbors(square)
+		day.memoryMap = append(day.memoryMap, square)
 
 		switch day.direction {
 		case "up":
@@ -43,6 +53,7 @@ func main() {
 			day.coordinateX = day.coordinateX + 1
 			break
 		}
+
 		day.stepsInTheCurrentDirection = day.stepsInTheCurrentDirection + 1
 
 		if day.stepsInTheCurrentDirection == day.maxStepsInThisDirection {
@@ -51,8 +62,61 @@ func main() {
 		}
 
 	}
-	fmt.Println("steps:", math.Abs(float64(day.coordinateX))+math.Abs(float64(day.coordinateY)))
-	// 419 is the correct answer for my input
+	for index, value := range day.memoryMap {
+		fmt.Printf("index: %v, value: %v\n", index, value)
+	}
+	// my correct answer is 295229
+}
+
+func (this *Day03) checkNeighbors(center Coordinate) int {
+	topLeft := Coordinate{x: center.x - 1, y: center.y + 1}
+	topMid := Coordinate{x: center.x, y: center.y + 1}
+	topRight := Coordinate{x: center.x + 1, y: center.y + 1}
+	midLeft := Coordinate{x: center.x - 1, y: center.y}
+	midRight := Coordinate{x: center.x + 1, y: center.y}
+	bottomLeft := Coordinate{x: center.x - 1, y: center.y - 1}
+	bottomMid := Coordinate{x: center.x, y: center.y - 1}
+	bottomRight := Coordinate{x: center.x + 1, y: center.y - 1}
+
+	sumOfNeighbors := 0
+	if value := contains(this.memoryMap, topLeft); value > 0 {
+		sumOfNeighbors = sumOfNeighbors + value
+	}
+	if value := contains(this.memoryMap, topMid); value > 0 {
+		sumOfNeighbors = sumOfNeighbors + value
+	}
+	if value := contains(this.memoryMap, topRight); value > 0 {
+		sumOfNeighbors = sumOfNeighbors + value
+	}
+	if value := contains(this.memoryMap, midLeft); value > 0 {
+		sumOfNeighbors = sumOfNeighbors + value
+	}
+	if value := contains(this.memoryMap, midRight); value > 0 {
+		sumOfNeighbors = sumOfNeighbors + value
+	}
+	if value := contains(this.memoryMap, bottomLeft); value > 0 {
+		sumOfNeighbors = sumOfNeighbors + value
+	}
+	if value := contains(this.memoryMap, bottomMid); value > 0 {
+		sumOfNeighbors = sumOfNeighbors + value
+	}
+	if value := contains(this.memoryMap, bottomRight); value > 0 {
+		sumOfNeighbors = sumOfNeighbors + value
+	}
+	if sumOfNeighbors == 0 {
+		sumOfNeighbors = 1
+	}
+
+	return sumOfNeighbors
+}
+
+func contains(slice []Coordinate, check Coordinate) int {
+	for _, value := range slice {
+		if check.x == value.x && check.y == value.y {
+			return value.value
+		}
+	}
+	return 0
 }
 
 func (this *Day03) changeDirection() {

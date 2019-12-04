794c17b4c922bc4db2841942f99906b3e1b26b2c
Jonathan Duncan <jonathan@nacnud.com>
2019-12-04 01:24:00 -0700

    2019, day 01, part 02

--- 2019/day-01/main.go
+++ 2019/day-01/main.go
@@ -13,15 +13,36 @@ func main() {
 		fmt.Println(err)
 	}
 	scanner := bufio.NewScanner(file)
-	var totalMass int
-	totalMass = 0
+	var totalFuel, totalFuelPlusFuel int
+	totalFuel = 0
 	for scanner.Scan() {
-		number, err := strconv.Atoi(scanner.Text())
+		moduleMass, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			fmt.Println(err)
 		}
 		// floor(mass/3) -2 = ?
-		totalMass = totalMass + number/3-2
+		massFuel, fuelFuel := calculateFuel(moduleMass)
+		totalFuel = totalFuel + massFuel
+		totalFuelPlusFuel = totalFuelPlusFuel + fuelFuel
 	}
-	fmt.Println("total mass:", totalMass)
+	fmt.Println("total mass fuel:", totalFuel)
+	fmt.Println("total fuel fuel:", totalFuelPlusFuel)
+	fmt.Println("grand total:", totalFuel + totalFuelPlusFuel)
+}
+
+func calculateFuel(mass int) (int,int) {
+	massFuel := mass/3-2
+	if mass < 0 || massFuel < 0 {
+		return mass, 0
+	}
+	fuelFuel := 0
+	fuelCounter := massFuel
+	for {
+		fuelCounter = fuelCounter/3-2
+		if fuelCounter < 0 {
+			break
+		}
+		fuelFuel = fuelFuel + fuelCounter
+	}
+	return massFuel, fuelFuel
 }

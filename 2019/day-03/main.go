192a976e38b180d3692369ba0592c228815f1f0b
Jonathan Duncan <jonathan@nacnud.com>
2019-12-18 00:07:28 -0700

    2019, day 03, part 01

---
+++ 2019/day-03/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+type coordinate struct {
+	x, y int
+}
+
+func main() {
+	bytes, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	// input has two lines, representing two wires
+	//bytes = []byte("R8,U5,L5,D3\nU7,R6,D4,L4")
+	//bytes = []byte("R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83") // = 159
+	//bytes = []byte("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7") // = 135
+	fileString := string(bytes)
+	wires := strings.Split(fileString, "\n")
+	wireOne := strings.Split(wires[0], ",")
+	wireTwo := strings.Split(wires[1], ",")
+
+	wireOnePath := pathFinder(wireOne)
+	wireTwoPath := pathFinder(wireTwo)
+
+	matches := findMatchingCoordinates(wireOnePath, wireTwoPath)
+	fmt.Println("closest distance to central port:", closestToZero(matches))
+}
+
+func closestToZero(list []int) int {
+	closest := 0
+	for _, x := range list {
+		if x == 0 {
+			continue
+		}
+		if x < closest {
+			closest = x
+		}
+		if closest == 0 {
+			closest = x
+		}
+	}
+	return closest
+}
+
+func findMatchingCoordinates(wireOnePath, wireTwoPath []coordinate) []int {
+	var matches []int
+	for _,w1p := range wireOnePath {
+		for _,w2p := range wireTwoPath {
+			if w1p == w2p {
+				if w1p.x < 0 {
+					w1p.x = -w1p.x
+				}
+				if w1p.y < 0 {
+					w1p.y = -w1p.y
+				}
+				matches = append(matches, w1p.x + w1p.y)
+			}
+		}
+	}
+	return matches
+}
+
+func pathFinder(input []string) []coordinate {
+	path := []coordinate{{0, 0}}
+	for _, vector := range input {
+		direction := vector[0]
+		magnitude, _ := strconv.Atoi(vector[1:])
+		if direction == Down {
+			path = travelDown(path, magnitude)
+		}
+		if direction == Left {
+			path = travelLeft(path, magnitude)
+		}
+		if direction == Right {
+			path = travelRight(path, magnitude)
+		}
+		if direction == Up {
+			path = travelUp(path, magnitude)
+		}
+	}
+	return path
+}
+
+func travelUp(path []coordinate, magnitude int) []coordinate {
+	for x := 0; x < magnitude; x++ {
+		path = append(path, coordinate{
+			x: path[len(path)-1].x,
+			y: path[len(path)-1].y + 1,
+		})
+	}
+	return path
+}
+func travelLeft(path []coordinate, magnitude int) []coordinate {
+	for x := 0; x < magnitude; x++ {
+		path = append(path, coordinate{
+			x: path[len(path)-1].x-1,
+			y: path[len(path)-1].y,
+		})
+	}
+	return path
+}
+func travelRight(path []coordinate, magnitude int) []coordinate {
+	for x := 0; x < magnitude; x++ {
+		path = append(path, coordinate{
+			x: path[len(path)-1].x + 1,
+			y: path[len(path)-1].y,
+		})
+	}
+	return path
+}
+func travelDown(path []coordinate, magnitude int) []coordinate {
+	for x := 0; x < magnitude; x++ {
+		path = append(path, coordinate{
+			x: path[len(path)-1].x,
+			y: path[len(path)-1].y - 1,
+		})
+	}
+	return path
+}
+
+
+
+const (
+	Down  = 68 // -y
+	Left  = 76 // -x
+	Right = 82 // +x
+	Up    = 85 // +y
+)

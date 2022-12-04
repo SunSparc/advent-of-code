7f587a8cf13cd797967d8d14720ac13d7023252b
Jonathan Duncan <jonathan@nacnud.com>
2022-12-03 20:11:26 -0700

    2022 Day 02 Part 2

--- 2022/day-02/main.go
+++ 2022/day-02/main.go
@@ -8,77 +8,101 @@ import (
 
 func main() {
 	var score int
-	for _, round := range loadInput() {
-		shapes := strings.Split(round, " ")
-		me := getValue(shapes[1])
+	//test := []string{
+	//	"A Y",
+	//	"B X",
+	//	"C Z",
+	//}
 
-		if (shapes[0] == "A" && shapes[1] == "X") ||
-			(shapes[0] == "B" && shapes[1] == "Y") ||
-			(shapes[0] == "C" && shapes[1] == "Z") {
-			// draw = 3
-			log.Printf("[DRAW] score(%v) + draw(3) + shape(%v)", score, me)
-			score = score + DRAW + me
-		} else if (shapes[0] == "A" && shapes[1] == "Y") ||
-			(shapes[0] == "B" && shapes[1] == "Z") ||
-			(shapes[0] == "C" && shapes[1] == "X") {
-			// win = 6
-			log.Printf("[WIN] score(%v) + win(6) + shape(%v)", score, me)
-			score = score + WIN + me
-		} else if (shapes[0] == "A" && shapes[1] == "Z") ||
-			(shapes[0] == "B" && shapes[1] == "X") ||
-			(shapes[0] == "C" && shapes[1] == "Y") {
-			// lose = 0
-			log.Printf("[LOSE] score(%v) + lose(0) + shape(%v)", score, me)
-			score = score + LOSE + me
+	for _, round := range loadInput() {
+
+		shapes := strings.Split(round, " ")
+
+		elfKey := shapes[0]
+		outcomeKey := shapes[1]
+		if elfKey == "A" && outcomeKey == DRAW {
+			// ROCK(A) vs DRAW(Y=3) = ROCK(A=1)
+			score = score + drawInt + 1
 		}
+
+		if elfKey == "B" && outcomeKey == LOSE {
+			// PAPER(B) vs LOSE(X=0) = ROCK(A=1)
+			score = score + loseInt + 1
+		}
+
+		if elfKey == "C" && outcomeKey == WIN {
+			// SCISSORS(C) vs WIN(Z=6) = ROCK(A=1)
+			score = score + winInt + 1
+		}
+
+		if elfKey == "B" && outcomeKey == DRAW {
+			// PAPER(B) vs DRAW(Y=3) = PAPER(B=2)
+			score = score + drawInt + 2
+		}
+
+		if elfKey == "C" && outcomeKey == LOSE {
+			// SCISSORS(C) vs LOSE(X=0) = PAPER(B=2)
+			score = score + loseInt + 2
+		}
+
+		if elfKey == "A" && outcomeKey == WIN {
+			// ROCK(A) vs WIN(Z=6) = PAPER(B=2)
+			score = score + winInt + 2
+		}
+
+		if elfKey == "C" && outcomeKey == DRAW {
+			// SCISSORS(C) vs DRAW(Y=3) = SCISSORS(C=3)
+			score = score + drawInt + 3
+		}
+
+		if elfKey == "A" && outcomeKey == LOSE {
+			// ROCK(A) vs LOSE(X=0) = SCISSORS(C=3)
+			score = score + loseInt + 3
+		}
+
+		if elfKey == "B" && outcomeKey == WIN {
+			// PAPER(B) vs WIN(Z=6) = SCISSORS(C=3)
+			score = score + winInt + 3
+		}
+
 	}
 	log.Println("score:", score) // 15632
 }
 
 func getValue(shape string) int {
 	switch shape {
-	case "A", "X":
+	case "A":
 		return 1
-	case "B", "Y":
+	case "B":
 		return 2
-	case "C", "Z":
+	case "C":
 		return 3
+	// 	WIN  = 6 // Z
+	//	DRAW = 3 // Y
+	//	LOSE = 0 // X
 	default:
 		log.Fatalln("[ERROR] unknown shape:", shape)
 		return 0
 	}
 }
 
-// rock vs rock = draw
-// rock vs paper = lose
-// rock vs scissors = win
-
-// paper vs paper = draw
-// paper vs scissors = lose
-// paper vs rock = win
-
-// scissors vs scissors = draw
-// scissors vs rock = lose
-// scissors vs paper = win
-
-/////////////////////////////////
-
-// A vs X = draw
-// B vs Y = draw
-// C vs Z = draw
-
-// A vs Y = win
-// B vs Z = win
-// C vs X = win
-
-// A vs Z = lose
-// B vs X = lose
-// C vs Y = lose
+///////////////////////////////////
 
 const (
-	WIN  = 6
-	DRAW = 3
-	LOSE = 0
+	WIN     = "Z" // 6 // Z
+	DRAW    = "Y" // 3 // Y
+	LOSE    = "X" // 0 // X
+	winInt  = 6   // Z
+	drawInt = 3   // Y
+	loseInt = 0   // X
+)
+
+var (
+	outcomes = map[string]int{
+		"Z": 6,
+		"Y": 3,
+		"X": 0,
+	}
 )
 
 ////////////////////
